e1bcc8f666ec0d1f86c003c84164b328df0e2206
Fearimdly <mosicou@163.com>
2020-10-09 14:40:47 +0800

    chore(*): moved dependencies to root project

--- app/src/main/java/com/worktile/lib/JsonTestOp.kt
+++ app/src/main/java/com/worktile/lib/JsonTestOp.kt
@@ -6,7 +6,7 @@ import com.worktile.json.operator.parse
 class TestOp {
     var value1 = 2
     var value2: List<Any> = emptyList()
-    var value3: Any? = null
+    var value3: Int? = null
     var value4: User? = null
     var value5: List<User> = emptyList()
 
@@ -20,10 +20,11 @@ class TestOp {
                 "678" > ::value2 {
 
                 }
-            } > ::value4
+            }
             "user" <User> {
                 "name" > it::name
             } > ::value4
+            "bbb" > ::value5
         }
     }
 }
